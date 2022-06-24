3ffc3ca307f2d46979899b4b6c06f02adca492ff
maihaiduong2015@outlook.com <maihaiduong2015@outlook.com>
2022-06-24 18:22:56 +0700

    Add new project

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,12 @@
+import Header from '~/components/Layout/Header';
+
+function Upload() {
+    return (
+        <div>
+            <Header />
+            <h2>upload</h2>
+        </div>
+    );
+}
+
+export default Upload;
